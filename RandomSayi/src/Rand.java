df896ec5cb7cebfe4969a9f7cbafbb9c93d747ef
Nihat Basmacı <nihatbasmaci@hotmail.com>
2014-11-20 19:57:33 +0200

    Random Metodu

---
+++ RandomSayi/src/Rand.java
@@ -0,0 +1,13 @@
+
+import java.util.Random;
+public class Rand {
+
+	
+	
+	public static void main(String[] args) {
+		Random rand1=new Random();
+		System.out.println(rand1.nextInt(10)+1);
+
+	}
+
+}
